b59bfccf32b6d24c02ae1c9c6e56cac05582217d
Dieguitux <andongue.nena@gmail.com>
2026-06-24 23:43:05 +0200

    update deps

--- rss-generator/app/build.gradle.kts
+++ rss-generator/app/build.gradle.kts
@@ -8,6 +8,10 @@ repositories {
 }
 
 dependencies {
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.kotlin.xml.builder)
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation(libs.junit.jupiter.engine)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
