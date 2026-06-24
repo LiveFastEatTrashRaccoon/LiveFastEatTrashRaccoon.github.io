a9b5f20741ca0d7d02d72a6b2d36f034603c28ab
Dieguitux <andongue.nena@gmail.com>
2026-06-24 23:47:01 +0200

    update Main.kt

--- rss-generator/app/src/main/kotlin/com/livefast/eattrash/rssgenerator/Main.kt
+++ rss-generator/app/src/main/kotlin/com/livefast/eattrash/rssgenerator/Main.kt
@@ -1,10 +1,37 @@
 package com.livefast.eattrash.rssgenerator
 
-import java.io.File
+import com.livefast.eattrash.rssgenerator.core.di.coreModule
+import com.livefast.eattrash.rssgenerator.data.PostRepository
+import com.livefast.eattrash.rssgenerator.data.di.dataModule
+import com.livefast.eattrash.rssgenerator.domain.FilePrinter
+import com.livefast.eattrash.rssgenerator.domain.RssGenerator
+import com.livefast.eattrash.rssgenerator.domain.di.domainModule
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.inject
 
 /**
  * Application entry point.
  */
 fun main() {
+    setupDI()
 
+    val repository: PostRepository by inject(PostRepository::class.java)
+    val generator: RssGenerator by inject(RssGenerator::class.java)
+    val printer: FilePrinter by inject(FilePrinter::class.java)
+
+    App(
+        repository = repository,
+        generator = generator,
+        printer = printer,
+    ).run()
+}
+
+private fun setupDI() {
+    startKoin {
+        modules(
+            coreModule,
+            dataModule,
+            domainModule,
+        )
+    }
 }
