b3d11b71d757b0cf57821d28b049b268f82e4ff6
Dieguitux <andongue.nena@gmail.com>
2026-06-25 21:53:38 +0200

    document FilePrinter and make file private

--- rss-generator/app/src/main/kotlin/com/livefast/eattrash/rssgenerator/App.kt
+++ rss-generator/app/src/main/kotlin/com/livefast/eattrash/rssgenerator/App.kt
@@ -3,7 +3,6 @@ package com.livefast.eattrash.rssgenerator
 import com.livefast.eattrash.rssgenerator.data.PostRepository
 import com.livefast.eattrash.rssgenerator.domain.FilePrinter
 import com.livefast.eattrash.rssgenerator.domain.RssGenerator
-import java.io.File
 
 class App(
     private val repository: PostRepository,
@@ -13,11 +12,6 @@ class App(
     fun run() {
         val posts = repository.getAll()
         val feedContent = generator.execute(posts = posts)
-        val destination = File(OUTPUT_FILE)
-        printer.execute(content = feedContent, destination = destination)
-    }
-
-    companion object {
-        private const val OUTPUT_FILE = "docs/blog/rss.xml"
+        printer.execute(content = feedContent)
     }
 }
