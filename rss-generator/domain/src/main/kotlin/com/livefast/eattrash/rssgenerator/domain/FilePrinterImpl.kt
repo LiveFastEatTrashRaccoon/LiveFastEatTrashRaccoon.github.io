b083843cec8c16550fd74b1ba6c85c9c1826e9d3
Dieguitux <andongue.nena@gmail.com>
2026-06-29 21:51:24 +0200

    add test for FilePrinterImpl

--- rss-generator/domain/src/main/kotlin/com/livefast/eattrash/rssgenerator/domain/FilePrinterImpl.kt
+++ rss-generator/domain/src/main/kotlin/com/livefast/eattrash/rssgenerator/domain/FilePrinterImpl.kt
@@ -3,18 +3,14 @@ package com.livefast.eattrash.rssgenerator.domain
 import java.io.File
 
 internal class FilePrinterImpl : FilePrinter {
-    override fun execute(content: String) {
-        val destination = File(OUTPUT_FILE_PATH)
+    override fun execute(
+        content: String,
+        destinationPath: String,
+    ) {
+        val destination = File(destinationPath)
         destination.parentFile?.mkdirs()
         destination.writer(charset = Charsets.UTF_8).use { writer ->
             writer.write(content)
         }
-    }
-
-    companion object {
-        /**
-         * Relative destination path where the RSS feed XML file is saved.
-         */
-        private const val OUTPUT_FILE_PATH = "docs/blog/rss.xml"
     }
 }
