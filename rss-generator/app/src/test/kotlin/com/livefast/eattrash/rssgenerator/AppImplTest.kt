6251487f82dd1a6da93d24f9846ee7d753b3e61f
Dieguitux <andongue.nena@gmail.com>
2026-06-30 22:14:44 +0200

    update App

--- rss-generator/app/src/test/kotlin/com/livefast/eattrash/rssgenerator/AppImplTest.kt
+++ rss-generator/app/src/test/kotlin/com/livefast/eattrash/rssgenerator/AppImplTest.kt
@@ -4,11 +4,13 @@ import com.livefast.eattrash.rssgenerator.core.data.PostRepository
 import com.livefast.eattrash.rssgenerator.core.model.PostData
 import com.livefast.eattrash.rssgenerator.domain.FilePrinter
 import com.livefast.eattrash.rssgenerator.domain.RssGenerator
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
-import io.mockk.verify
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
 class AppImplTest {
@@ -16,27 +18,29 @@ class AppImplTest {
     private val rssGenerator = mockk<RssGenerator>()
     private val filePrinter = mockk<FilePrinter>()
 
-    private val sut =
-        AppImpl(
-            repository = postRepository,
-            generator = rssGenerator,
-            printer = filePrinter,
-        )
+    private lateinit var sut: App
 
     @Test
-    fun `when run is invoked, then it executes the logic`() {
-        val data = listOf<PostData>()
-        val content = "fake-content"
-        every { postRepository.getAll() } returns data
-        every { rssGenerator.execute(any()) } returns content
-        every { filePrinter.execute(any()) } just runs
+    fun `when run is invoked, then it executes the logic`() =
+        runTest {
+            sut =
+                AppImpl(
+                    repository = postRepository,
+                    generator = rssGenerator,
+                    printer = filePrinter,
+                )
+            val data = listOf<PostData>()
+            val content = "fake-content"
+            coEvery { postRepository.getAll() } returns data
+            every { rssGenerator.execute(any()) } returns content
+            coEvery { filePrinter.execute(any()) } just runs
 
-        sut.run()
+            sut.run()
 
-        verify {
-            postRepository.getAll()
-            rssGenerator.execute(data)
-            filePrinter.execute(content)
+            coVerify {
+                postRepository.getAll()
+                rssGenerator.execute(data)
+                filePrinter.execute(content)
+            }
         }
-    }
 }
