fb68e7b859d885423fff2e5eb53707e575d72939
Dieguitux <andongue.nena@gmail.com>
2026-06-30 22:05:25 +0200

    update PostRepository

--- rss-generator/core/data/src/main/kotlin/com/livefast/eattrash/rssgenerator/core/data/PostRepositoryImpl.kt
+++ rss-generator/core/data/src/main/kotlin/com/livefast/eattrash/rssgenerator/core/data/PostRepositoryImpl.kt
@@ -3,22 +3,25 @@ package com.livefast.eattrash.rssgenerator.core.data
 import com.livefast.eattrash.rssgenerator.core.data.converter.PostDtoToModelConverter
 import com.livefast.eattrash.rssgenerator.core.data.dto.MetadataRootDto
 import com.livefast.eattrash.rssgenerator.core.model.PostData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.mamoe.yamlkt.Yaml
 
 internal class PostRepositoryImpl(
     private val converter: PostDtoToModelConverter,
 ) : PostRepository {
-    override fun getAll(): List<PostData> {
-        val root =
-            PostRepositoryImpl::class.java
-                .getResourceAsStream(RESOURCE_PATH)
-                ?.use { stream ->
-                    val content = stream.readAllBytes().toString(charset = Charsets.UTF_8)
-                    Yaml.decodeFromString(deserializer = MetadataRootDto.serializer(), string = content)
-                }
-        val result = root?.metadata?.map { dto -> converter.convert(dto) }
-        return result.orEmpty()
-    }
+    override suspend fun getAll(): List<PostData> =
+        withContext(Dispatchers.IO) {
+            val root =
+                PostRepositoryImpl::class.java
+                    .getResourceAsStream(RESOURCE_PATH)
+                    ?.use { stream ->
+                        val content = stream.readAllBytes().toString(charset = Charsets.UTF_8)
+                        Yaml.decodeFromString(deserializer = MetadataRootDto.serializer(), string = content)
+                    }
+            val result = root?.metadata?.map { dto -> converter.convert(dto) }
+            result.orEmpty()
+        }
 
     companion object {
         private const val RESOURCE_PATH = "/metadata.yml"
